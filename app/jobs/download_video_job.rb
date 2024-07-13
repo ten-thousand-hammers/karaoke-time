c580ef298fb64dc33ee9851ce3134c366f77779b
Nathan Palmer <nathanpalmer@users.noreply.github.com>
2024-07-13 17:19:47 -0400

    If it's already downloaded, well, don't try again

--- app/jobs/download_video_job.rb
+++ app/jobs/download_video_job.rb
@@ -4,11 +4,24 @@ class DownloadVideoJob < ApplicationJob
   def perform(song)
     return if song.downloaded
 
+    extension = "mp4"
+    if File.exist?(File.join("public", "videos", "#{song.external_id}.#{extension}"))
+      song.update!(
+        downloaded: true,
+        File.join("videos", "#{song.external_id}.#{file_quality}")
+      )
+      return
+    end
+
     video_url = "https://www.youtube.com/watch?v=#{song.external_id}"
     download_path = File.join("public", "videos", "%(id)s.%(ext)s")
-    file_quality = "mp4"
-    # file_quality = "bestvideo[ext!=webm][height<=1080]+bestaudio[ext!=webm]/best[ext!=webm]"
-    cmd = ["yt-dlp", "-f", file_quality, "-o", "\"#{download_path}\"", video_url].join(" ")
+    file_quality = extension # "bestvideo[ext!=webm][height<=1080]+bestaudio[ext!=webm]/best[ext!=webm]"
+    cmd = [
+      "yt-dlp",
+      "-f", file_quality,
+      "-o", "\"#{download_path}\"", video_url
+    ].join(" ")
+
     stdout_and_stderr, status = Open3.capture2e(cmd)
     exit_code = status.exitstatus
 
