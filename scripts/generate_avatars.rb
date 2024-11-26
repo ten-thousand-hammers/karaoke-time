af3a2ec401b37e08a42557b7c1b0427d2216c5ef
Nathan Palmer <nathanpalmer@users.noreply.github.com>
2024-11-25 22:16:43 -0500

    chore: Getting some different avatars

--- scripts/generate_avatars.rb
+++ scripts/generate_avatars.rb
@@ -6,7 +6,7 @@ AVATAR_DIR = File.join(File.dirname(__FILE__), '../public/images/avatars')
 FileUtils.mkdir_p(AVATAR_DIR)
 
 # Using DiceBear's "adventurer" style which creates fun, consistent avatars
-STYLES = ['adventurer']
+STYLES = ['adventurer-neutral']
 
 puts "Generating #{AVATAR_COUNT} avatars..."
 
@@ -17,7 +17,7 @@ AVATAR_COUNT.times do |i|
   # Generate a consistent seed for each avatar number
   seed = "karaoketime#{avatar_num}"
   
-  url = "https://api.dicebear.com/7.x/#{style}/png?seed=#{seed}&backgroundColor=ffdfbf,ffd5dc,c0aede,bde4f4,b6e3f4"
+  url = "https://api.dicebear.com/9.x/#{style}/png?seed=#{seed}&backgroundColor=ffdfbf,ffd5dc,c0aede,bde4f4,b6e3f4"
   output_path = File.join(AVATAR_DIR, "avatar_#{avatar_num}.png")
   
   puts "Downloading avatar #{avatar_num}..."
