855c1f5fccd62d350790a95fe1da42700057c002
Nathan Palmer <nathanpalmer@users.noreply.github.com>
2024-12-15 21:20:36 -0500

    Revert "fix: Let's just use a random ID for now, because fingerprintjs isn't working on safari"
    This reverts commit 1410d05b9ce65b7cce71337d22d67184132ba365.

--- app/controllers/concerns/secured.rb
+++ app/controllers/concerns/secured.rb
@@ -28,9 +28,9 @@ module Secured
   end
 
   def logged_in_using_browser_id?
-    return unless browser_id.present?
+    return unless cookies[:_karaoke_time_browser_id].present?
 
-    user = User.find_or_initialize_by(browser_id: browser_id)
+    user = User.find_or_initialize_by(browser_id: cookies[:_karaoke_time_browser_id])
     if user.new_record?
       user.save!
       redirect_to edit_profile_path(user), notice: "Looks like it's the first time you're using Karaoke Time. Please add a Nickname and Avatar."
--- app/javascript/controllers/browser_id_controller.js
+++ app/javascript/controllers/browser_id_controller.js
@@ -1,54 +1,29 @@
 import { Controller } from "@hotwired/stimulus"
-// import FingerprintJS from '@fingerprintjs/fingerprintjs'
+import FingerprintJS from '@fingerprintjs/fingerprintjs'
 
 export default class extends Controller {
-
-
   connect() {
-    this.version = 1
     this.load();
   }
 
   async load() {
     if (document.cookie.indexOf('_karaoke_time_browser_id') > -1) {
-      var cookieValue = document.cookie['_karaoke_time_browser_id'];
-      if (typeof cookieValue !== 'Object') {
-        this.removeCookie('_karaoke_time_browser_id');
-        cookieValue = {}
-      }
-
-      if (cookieValue.version === this.version) {
-        return;
-      }
-      this.removeCookie('_karaoke_time_browser_id');
-      cookieValue = {}
+      return;
     }
 
-    // const fpPromise = await FingerprintJS.load()
-    // const fp = await fpPromise.get()
+    const fpPromise = await FingerprintJS.load()
+    const fp = await fpPromise.get()
 
-    var uniqueId;
-    // var uniqueId = fp.visitorId;
+    var uniqueId = fp.visitorId;
     if (uniqueId == null) {
       uniqueId = crypto.randomUUID();
     }
 
-    this.setCookie(
-      '_karaoke_time_browser_id',
-      JSON.stringify({
-        version: this.version,
-        id: uniqueId
-      }),
-      30 // Cookie expires in 30 days
-    );
+    this.setCookie('_karaoke_time_browser_id', uniqueId, 30); // Cookie expires in 30 days
   }
 
   setCookie(name, value, days) {
     const expires = new Date(Date.now() + days * 864e5).toUTCString();
     document.cookie = name + '=' + encodeURIComponent(value) + '; expires=' + expires + '; path=/';
-  }
-
-  removeCookie(name) {
-    document.cookie = name + '=; Max-Age=-99999999;';
   }
 }
