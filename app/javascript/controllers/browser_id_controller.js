fed697d9981fe9c681e7f7808b42a9e1094d1c61
Nathan Palmer <nathanpalmer@users.noreply.github.com>
2024-12-15 22:39:43 -0500

    chore: Comment out debugging here

--- app/javascript/controllers/browser_id_controller.js
+++ app/javascript/controllers/browser_id_controller.js
@@ -11,24 +11,24 @@ export default class extends Controller {
 
   async load() {
     if (this.cookieExists('_karaoke_time_browser_id')) {
-      console.log('Browser ID already exists');
+      // console.log('Browser ID already exists');
       var cookieValue = this.getCookie('_karaoke_time_browser_id');
       if (cookieValue.indexOf("{") === 0 || cookieValue.indexOf("%7B") === 0) {
         cookieValue = JSON.parse(decodeURIComponent(cookieValue));
       }
 
       if (typeof cookieValue !== 'object') {
-        console.log(`Browser ID is not an object: ${typeof cookieValue} - ${cookieValue}`);
+        // console.log(`Browser ID is not an object: ${typeof cookieValue} - ${cookieValue}`);
         this.removeCookie('_karaoke_time_browser_id');
         cookieValue = {}
       }
 
       if (cookieValue.version === this.version) {
-        console.log(`Browser ID version is up to date: ${cookieValue.id}`);
+        // console.log(`Browser ID version is up to date: ${cookieValue.id}`);
         return;
       }
 
-      console.log('Browser ID version is outdated');
+      // console.log('Browser ID version is outdated');
       this.removeCookie('_karaoke_time_browser_id');
       cookieValue = {}
     }
