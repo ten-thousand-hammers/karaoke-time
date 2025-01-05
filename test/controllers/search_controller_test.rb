cc1f0aa78cec63891a1766e6c5d45a207376c639
Nathan Palmer <nathanpalmer@users.noreply.github.com>
2025-01-05 10:56:28 -0500

    test: Adding tests for search

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -1,8 +1,41 @@
 require "test_helper"
 
 class SearchControllerTest < ActionDispatch::IntegrationTest
-  # test "should get index" do
-  #   get search_index_url
-  #   assert_response :success
-  # end
+  test "should get index" do
+    get search_url
+    assert_response :success
+  end
+
+  test "can search for a song" do
+    get search_url, params: { search: "Oasis" }
+    assert_response :success
+    assert_select "turbo-frame#results .grid" do
+      assert_select "turbo-frame", 10
+    end
+  end
+
+  test "can play a search that was searched for" do
+    get search_url, params: { search: "Oasis" }
+    assert_response :success
+    song = Song.first
+
+    assert_enqueued_with(job: QueueVideoJob) do
+      post play_url, params: { id: song.external_id }
+      assert_response :success
+    end
+  end
+
+  test "can play a search that was searched for with turbo" do
+    get search_url, params: { search: "Oasis" }
+    assert_response :success
+    song = Song.first
+
+    assert_enqueued_with(job: QueueVideoJob) do
+      post play_url, params: {
+        id: song.external_id
+      }, as: :turbo_stream
+      assert_response :success
+      assert_select "turbo-stream[action=prepend][target=flash]", text: "#{song.name} has been queued"
+    end
+  end
 end
