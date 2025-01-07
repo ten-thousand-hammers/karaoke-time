f7efeb7279a9ec7829e7079dcf6556eef1369f82
Nathan Palmer <nathanpalmer@users.noreply.github.com>
2025-01-06 22:48:27 -0500

    build: Adding no-sandbox and --disable-dev-shm-usage to selenium to fix the chrome crash

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,14 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  if ENV["CI"]
+    driven_by :selenium, using: :headless_chrome do |option|
+      option.add_argument("no-sandbox")
+      option.add_argument("--disable-dev-shm-usage")
+    end
+  else
+    driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  end
 
   def wait_for_turbo_frame(selector)
     if has_selector?("turbo-frame#{selector}[busy]", visible: true, wait: 0.25.seconds)
