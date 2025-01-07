54fc5b3a51c409bda132b4a008a87103507b167e
Nathan Palmer <nathanpalmer@users.noreply.github.com>
2025-01-06 22:39:01 -0500

    test: Adding tests for queue video job

--- test/jobs/download_video_job_test.rb
+++ test/jobs/download_video_job_test.rb
@@ -3,54 +3,21 @@ require "test_helper"
 class DownloadVideoJobTest < ActiveJob::TestCase
   test "it download a video" do
     song = Song.create!(external_id: "123", name: "Test Song")
-    Open3.expects(:capture3).with(
-      YtDlpManager::BINARY_PATH,
-      "-f", "mp4",
-      "-o", File.join("public", "videos", "%(id)s.%(ext)s"),
-      "https://www.youtube.com/watch?v=#{song.external_id}"
-    ).returns(
-      [
-        "",
-        "",
-        stub(success?: true)
-      ]
-    )
+    mock_yt_download(song, returns: ["", "", stub(success?: true)])
     DownloadVideoJob.perform_now(song)
   end
 
   test "it updates the up next performance status after downloading" do
     song = Song.create!(external_id: "123", name: "Test Song")
     Performance.instance.update!(up_next_song: song, up_next_download_status: "")
-    Open3.expects(:capture3).with(
-      YtDlpManager::BINARY_PATH,
-      "-f", "mp4",
-      "-o", File.join("public", "videos", "%(id)s.%(ext)s"),
-      "https://www.youtube.com/watch?v=#{song.external_id}"
-    ).returns(
-      [
-        "",
-        "",
-        stub(success?: true)
-      ]
-    )
+    mock_yt_download(song, returns: ["", "", stub(success?: true)])
     DownloadVideoJob.perform_now(song)
     assert_equal "completed", Performance.instance.reload.up_next_download_status
   end
 
   test "it marks a failed download" do
     song = Song.create!(external_id: "123", name: "Test Song")
-    Open3.expects(:capture3).with(
-      YtDlpManager::BINARY_PATH,
-      "-f", "mp4",
-      "-o", File.join("public", "videos", "%(id)s.%(ext)s"),
-      "https://www.youtube.com/watch?v=#{song.external_id}"
-    ).returns(
-      [
-        "",
-        "An error occurred",
-        stub(success?: false)
-      ]
-    )
+    mock_yt_download(song, returns: ["", "An error occurred", stub(success?: false)])
     DownloadVideoJob.perform_now(song)
     assert_equal "An error occurred", song.reload.download_error
   end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,7 @@ require "mocha/minitest"
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
+    # parallelize(workers: :number_of_processors)
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
@@ -14,6 +14,15 @@ module ActiveSupport
     # Add more helper methods to be used by all tests here...
     def sign_in(user)
       cookies[:_karaoke_time_browser_id] = user.browser_id
+    end
+
+    def mock_yt_download(song, returns: ["", "", stub(success?: true)])
+      Open3.expects(:capture3).with(
+        YtDlpManager::BINARY_PATH,
+        "-f", "mp4",
+        "-o", File.join("public", "videos", "%(id)s.%(ext)s"),
+        "https://www.youtube.com/watch?v=#{song.external_id}"
+      ).returns(returns)
     end
   end
 end
